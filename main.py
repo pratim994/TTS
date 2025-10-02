4a6673b75a2df07149ed95b9820e52e418739e34
pratim chakraborty <aryanpratim86@gmail.com>
2025-10-02 12:42:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import torch
+from TTS.api import TTS
+import gradio as gr 
+
+device ="cuda" if torch.cuda.is_available() else "cpu"
+print("enter the text you want to transform")
+text = str(input())
+
+def generate_audio(text):
+    tts = TTS(model_name='tts_models/en/jenny/jenny').to(device)
+    tts.tts_to_file(text=text, file_path="outputs/output.wav")
+    return "outputs/output.wav"
+
+demo = gr.Interface(
+    fn=generate_audio,
+     inputs=[gr.Text(label="Text"),],
+     outputs=[gr.Audio(label="Audio"),],
+)
+
+demo.launch()
